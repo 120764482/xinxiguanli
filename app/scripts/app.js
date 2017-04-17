9d2b899fbca3bae2586837873bc5d717982cde05
120764482 <120764482@qq.com>
2017-04-17 18:36:45 +0800

    4.17

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name xinxiApp
+ * @description
+ * # xinxiApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('xinxiApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name xinxiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the xinxiApp
+ */
+angular.module('xinxiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
