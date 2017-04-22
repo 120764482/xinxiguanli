d5ed75d76e78e0ee0691fbe68d5cb37f366b4342
maruixue0217 <2454633461@qq.com>
2017-04-22 15:40:17 +0800

    5

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -6,7 +6,7 @@ angular.module('xinxiApp')
           	 $.ajax({
 			 	type: "get",
 			 	// 拼字符串：?userId+id  
-			 	url: "http://localhost:3560/list/list",	
+			 	url: "http://192.168.43.238:3560/list/list",	
 			 // 	data:{
 			 // 		userId:id,
 		 	// },		
@@ -14,27 +14,15 @@ angular.module('xinxiApp')
 			 		// console.log(e);
 			       var html='';
 			     for(var i=0;i<e.length;i++){      		    
-         //     html+='<tr><td><input value="'+e[i].name+'"></input></td><td><input value="'+e[i].classnames+'"></input></td>
-      		 // <td><input value="'+e[i].sex+'"></input></td><td><input value="'+e[i].cellphone+'"></input></td>
-      		 // <td><input value="'+e[i].address+'"></input></td><td><input value="'+e[i].teachername+'"></input></td>
-      		 // <td><input value="'+e[i].teachlaoshi+'"></input></td><td><input value="'+e[i].patriarchname+'"></input></td>
-      		 // <td><input value="'+e[i].patriarccellphone+'"></input></td><td><input value="'+e[i].dorm+'"></input></td>
-      		 // <td><input value="'+e[i].number+'"></input></td></tr>'
-      		 html+='<tr><td><input value="'+e[i].name+'"></input></td><td><input value="'+e[i].classnames+'"></input></td><td><input value="'+e[i].sex+'"></input></td><td><input value="'+e[i].cellphone+'"></input><td><input value="'+e[i].jineng+'"></input></td></tr>'
-      		
+      	 html+="<tr height='35px' click='btn' id='"+e[i].id+"'><td color:'#fff'>"+e[i].name+"</td><td color:'#fff'>"+e[i].classnames+"</td><td color:'#fff'>"+e[i].sex+"</td><td color:'#fff'>"+e[i].cellphone+"</td><td color:'#fff'>"+e[i].teachlaoshi+"</td><td color:'#fff'>"+e[i].patriarchname+"</td><td color:'#fff'>"+e[i].patriarccellphone+"</td><td color:'#fff'>"+e[i].dorm+"</td><td color:'#fff'>"+e[i].number+"</td></tr>"
+
 						}
-			$(".div").append(html);
+			$("#main").append(html);
 			 	},
 			 	error: function() {
 			 		alert("失败")
 			 	}
-			 })
-
-  // })
-        
-
-
-
+			 })      
    //   //搜索 
    // $('#sousuo').click(function() {
    //        	   var  str=$('.sousuo').val()
@@ -56,7 +44,26 @@ angular.module('xinxiApp')
 			// 	});
    //        })
 
-
+//详情
+            $('.btn').click(function() {
+          	   var  str=$('.sousuo').val()
+          		$.ajax({
+					type:"get",
+					url:"http://localhost:8005/sousuo/sousuo",
+					data:{
+						'val':str
+					},				
+					success:function(e){
+					console.log(e);
+					location.href="sousuo.html"
+				
+				},
+					error:function(){
+						alert('失敗')
+					 
+					}
+				});
+          })
 
 
 
