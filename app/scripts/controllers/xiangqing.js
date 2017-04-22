06bad59783089242df7f071cab2cc3bddf394d8b
liangzhen052789 <1695061020@qq.com>
2017-04-22 20:21:31 +0800

    qwe

--- app/scripts/controllers/xiangqing.js
+++ app/scripts/controllers/xiangqing.js
@@ -1,27 +1,27 @@
 angular.module('xinxiApp')
 	.controller('xiangCtrl', ["$scope", "$http", "$location", "$state", "$stateParams", function($scope, $http, $location,$routeParams, $stateParams, $state) {
-		var id = window.location.href.split('=')[1];
-		//var id = $routeParams.id;
-		console.log(id)
-//		$.ajax({
-//			url: "http://192.168.43.238:3560/geren/xinxi?Uid="+id,
-//			type: "get",
-//			success: function(reqs) {
-//				//console.log(reqs)
-//				$('.username').val(reqs[0].name);
-//				$('.theath').val(reqs[0].teachername);
-//				$('.class').val(reqs[0].name);
-//				$('.phone').val(reqs[0].cellphone);
-//				$('.address').val(reqs[0].address);
-//				$('.jiazhangName').val(reqs[0].patriarchname);
-//				$('.jiazhangPhone').val(reqs[0].patriarccellphone);
-//				$('.sushe').val(reqs[0].dorm);
-//				$('.id').val(reqs[0].studentUid);
-//				$('.jifen').val(reqs[0].number);
-//				$('.zhuanye').val(reqs[0].teacherlaoshi);
-//				$('.sex').val(reqs[0].sex);
-//			},
-//			error: function(data) {}
-//		})
+		//var id = window.location.href.split('g')[1];
+		var id = $stateParams.id;
+		//console.log(id)
+		$.ajax({
+			url: "http://192.168.43.238:3560/geren/xinxi?Uid="+id,
+			type: "get",
+			success: function(reqs) {
+				//console.log(reqs)
+				$('.username').val(reqs[0].name);
+				$('.theath').val(reqs[0].teachername);
+				$('.class').val(reqs[0].name);
+				$('.phone').val(reqs[0].cellphone);
+				$('.address').val(reqs[0].address);
+				$('.jiazhangName').val(reqs[0].patriarchname);
+				$('.jiazhangPhone').val(reqs[0].patriarccellphone);
+				$('.sushe').val(reqs[0].dorm);
+				$('.id').val(reqs[0].studentUid);
+				$('.jifen').val(reqs[0].number);
+				$('.zhuanye').val(reqs[0].teacherlaoshi);
+				$('.sex').val(reqs[0].sex);
+			},
+			error: function(data) {}
+		})
 
 	}])
